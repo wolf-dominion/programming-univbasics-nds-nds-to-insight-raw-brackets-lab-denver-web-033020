98384f75aaff1bed2e9af209bf3d2b65eb445ebe
Hannah Drury <hannah_drury@yahoo.com>
2020-02-04 00:23:03 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
@@ -18,5 +19,24 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
+ 
   nil
+  hash = {}
+  
+  row = 0
+  while row < nds.length do
+    value = 0
+    col = 0
+    key = nds[row][:name]
+    while col < nds[row][:movies].length do
+      value = value + nds[row][:movies][col][:worldwide_gross]
+      col += 1
+    end
+    row += 1
+    puts key
+    puts value
+    hash[key] = value
+  end
+  pp hash
+  hash
 end
